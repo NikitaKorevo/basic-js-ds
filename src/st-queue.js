8b8f3f71d75a280ab13b4a8420b168f8a9fea128
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2021-10-18 13:05:37 +0300

    feat: solve st-queue

--- src/st-queue.js
+++ src/st-queue.js
@@ -14,17 +14,35 @@ const { ListNode } = require('../extensions/list-node.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 module.exports = class Queue {
+  constructor() {
+    this.linkedList = null;
+  }
 
   getUnderlyingList() {
-
+    return this.linkedList;
   }
 
   enqueue(value) {
-    
+    if (!this.linkedList) {
+      return this.linkedList = new ListNode(value);
+    }
+
+    let current = this.linkedList;
+    while (current) {
+      if (!current.next) {
+        return current.next = new ListNode(value);
+      } else {
+        current = current.next;
+      }
+    }
   }
 
   dequeue() {
+    if (!this.linkedList) return null;
 
+    let firstValue = this.linkedList.value;
+    this.linkedList.value = this.linkedList.next.value;
+    this.linkedList.next = this.linkedList.next.next;
+    return firstValue;
   }
-
 }
